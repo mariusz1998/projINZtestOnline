d79c8dc127687217e7dddae1eda697d3db74afb4
Mariusz Drynda <mariusz1998@wp.pl>
2020-11-27 19:21:55 +0100

    pobranie liczby użytkowników (odpowiednie login i password) przy logowaniu

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use("/public1", express.static(__dirname + "/public1"));
 
 app.use("/public2", express.static(__dirname + "/public2"));
 
-var driver = neo4j.driver('bolt://54.237.207.193:32954', neo4j.auth.basic('neo4j','toe-employees-interests'));
+var driver = neo4j.driver('bolt://54.210.172.235:33238', neo4j.auth.basic('neo4j', 'dump-clerks-seconds'));
 
 var session = driver.session();
 
@@ -41,8 +41,9 @@ app.post('/registration/add',function(req,res){
        var password = req.body.password;
        var email = req.body.email;
     session123            
-            .run('CREATE(n:User {firstName:$firstNameParam,lastName:$lastNameParam,login:$loginParam,password:$passwordParam,email:$emailParam}) RETURN n.login',
-  {firstNameParam:firstName,lastNameParam:lastName,loginParam:login,passwordParam:password,emailParam:email})
+            .run('CREATE(n:User {login:\'Mariusz\',password:\'12345\',email:\'mariusz@email.pl\'})',)
+     // .run('CREATE(n:Admin {startDay:\'25-11-2020\',endDay:\'25-12-2020\'})',)
+     //.run('CREATE(n:File {name:\'csv1\',extension:\'csv\',url:\'url.com\'})',)
             .then(function(result){    
                   res.redirect('/registration');
                     // session123.close();
@@ -69,19 +70,20 @@ res.render('aktorzy',{actors: actorArr});
            console.log();         
         });
 });
-
-
 app.get('/login', function(req,res){ 
 session
-        .run('MATCH(n:User) RETURN n.login ')
+        .run('MATCH(n:User) RETURN n')
         .then(function(result2){
             var userArr= [];
+     
             result2.records.forEach(function(record){
                 userArr.push({
                     login: record._fields[0].properties.login,
                     password: record._fields[0].properties.password
+                    
                 });
             });
+         
 res.render('login',{users:userArr});
         })
                 .catch(function(err){
@@ -121,16 +123,35 @@ app.post('/movie/actor/add',function(req,res){
     var name = req.body.name; //do aktora
     var title = req.body.title;
     session123
-            .run('MATCH(a:Person{name:$nameParam}),(b:Movie{title:$titleParam}) MERGE(a)-[r:ACTED_IN {year:2012}]-(b) RETURN a,b'
-            ,{titleParam: title, nameParam:name})
-            //dodało year 2012
+           .run('MATCH(a:User{login:\'Marek\'}),(b:File{name:\'csv1\'}) MERGE(a)-[r:Owner]-(b) RETURN a,b')
+   //.run('MATCH(a:User{login:\'Marek\'}),(b:Admin{startDay:\'25-11-2020\'}) MERGE(b)-[r:ADMIN]-(a) RETURN a,b')
+          //   .run('MATCH(n)-[r:ADMIN]->()  DELETE r')    //dodało year 2012
                     .then(function(result){
-                        res.redirect('/aktorzy');
+                       res.redirect('/registration');
             })
                     .catch(function(err){
                         console.log(err);          
             });
 });
+
+app.post('/login/verify',function(req,res){
+    var login = req.body.login; //pobranie danych
+    var password = req.body.password;
+    session123
+            .run('MATCH (n:User{login:$loginParam,password:$yearParam}) RETURN count(n) as user_exists',
+  {loginParam:login,yearParam:password})
+        .then(function(result2){
+             console.log(result2.records[0].get('user_exists').low ); 
+          if(result2.records[0].get('user_exists').low===1)
+                res.render('loginSuccess'); 
+          else
+                 res.render('loginError');
+       })
+                    .catch(function(err){
+                        console.log(err);          
+            });
+});
+
 app.listen(3000);
 console.log('Server Started on Port 3000');
 
